bb72fc114434b916ce4c91966b8f8a58338d9541
ec221013 <williamdthai@gmail.com>
2024-03-13 11:01:19 +0000

    modified:   src/App.js; src/components/NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-
-import CurrentWeather from './components/CurrentWeather';
-import Alerts from './components/Alerts.js';
-import Statistics from './components/Statistics.js';
-import Suggestions from './components/Suggestions.js';
 
 import NavBar from './components/NavBar';
 
 function App(){
   return (
-    <Router>
       <div>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Navigate to = '/home'/>} />
-          <Route path="/home" element={<CurrentWeather />} />
-          <Route path="/alerts" element={<Alerts />} />
-          <Route path="/statistics" element={<Statistics />} />
-          <Route path="/suggestions" element={<Suggestions />} />
-        </Routes>
+
       </div>
-    </Router>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,35 @@
-// CropPlanner.js
+// navbar
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {  BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
+
+import CurrentWeather from './CurrentWeather';
+import Alerts from './Alerts.js';
+import Statistics from './Statistics.js';
+import Suggestions from './Suggestions.js';
 
 import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <nav id='background'>
-      <ul id='list'>
-        <li id='listitem'><Link to='/home'><img src='./components/assets/navbar/icon_home.png' alt="Home"></img></Link></li>
-        <li id='listitem'><Link to='/alerts'><img src='./components/assets/navbar/icon_check.png' alt="Alerts"></img></Link></li>
-        <li id='listitem'><Link to='/statistics'><img src='./components/assets/navbar/icon_check.png' alt="Stats"></img></Link></li>
-        <li id='listitem'><Link to='/suggestions'><img src='./components/assets/navbar/icon_check.png' alt="Suggest"></img></Link></li>
-      </ul>
-    </nav>
+    <Router>
+      <nav id='background'>
+        <ul id='list'>
+          <li id='listitem'><Link to='/home'><img src='./components/assets/navbar/icon_home.png' alt="Home"></img></Link></li>
+          <li id='listitem'><Link to='/alerts'><img src='./components/assets/navbar/icon_check.png' alt="Alerts"></img></Link></li>
+          <li id='listitem'><Link to='/statistics'><img src='./components/assets/navbar/icon_check.png' alt="Stats"></img></Link></li>
+          <li id='listitem'><Link to='/suggestions'><img src='./components/assets/navbar/icon_check.png' alt="Suggest"></img></Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+          <Route path="/" element={<Navigate to = '/home'/>} />
+          <Route path="/home" element={<CurrentWeather />} />
+          <Route path="/alerts" element={<Alerts />} />
+          <Route path="/statistics" element={<Statistics />} />
+          <Route path="/suggestions" element={<Suggestions />} />
+        </Routes>
+    </Router>
+
   );
 };
 
