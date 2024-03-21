6a04c52fd905ed2c7e0897c821f11ab3d1742533
ec221013 <ec221013@qmul.ac.uk>
2024-03-21 23:07:26 +0000

    button fix

--- src/components/SuggestionsHeader.js
+++ src/components/SuggestionsHeader.js
@@ -7,16 +7,10 @@ const SuggestionsHeader = ({ setCurrentScreen }) => {
     <>
       <nav className='s-header'>
         <ul className='s-list'>
-          <button className="s-default" onClick={() => setCurrentScreen('planner')}>Planner</button>
-          <button className="s-default" onClick={() => setCurrentScreen('suggestions')}>Suggestions</button>
+          <button className="s-button" onClick={() => setCurrentScreen('planner')}>Planner</button>
+          <button className="s-button" onClick={() => setCurrentScreen('suggestions')}>Suggestions</button>
         </ul>
       </nav>
-
-      {/* <div className="header">
-        <button className="planner-button" onClick={() => setCurrentScreen('planner')}>Planner</button>
-        <button className="suggestions-button" onClick={() => setCurrentScreen('suggestions')}>Suggestions</button>
-      </div> */}
-
     </>
   );
 };
