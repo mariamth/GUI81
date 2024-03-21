e9dbdd9c9dafa4386ce59a847fbb2252c8f2186b
ec221013 <ec221013@qmul.ac.uk>
2024-03-21 14:15:52 +0000

    s header

--- src/components/SuggestionsHeader.js
+++ src/components/SuggestionsHeader.js
@@ -4,10 +4,20 @@ import './SuggestionsHeader.css';
 
 const SuggestionsHeader = ({ setCurrentScreen }) => {
   return (
-    <div className="header">
-      <button className="planner-button" onClick={() => setCurrentScreen('planner')}>Planner</button>
-      <button className="suggestions-button" onClick={() => setCurrentScreen('suggestions')}>Suggestions</button>
-    </div>
+    <>
+      <nav className='s-header'>
+        <ul className='s-list'>
+          <button className="s-default" onClick={() => setCurrentScreen('planner')}>Planner</button>
+          <button className="s-default" onClick={() => setCurrentScreen('suggestions')}>Suggestions</button>
+        </ul>
+      </nav>
+
+      {/* <div className="header">
+        <button className="planner-button" onClick={() => setCurrentScreen('planner')}>Planner</button>
+        <button className="suggestions-button" onClick={() => setCurrentScreen('suggestions')}>Suggestions</button>
+      </div> */}
+
+    </>
   );
 };
 
