fe7f4292b522af9de48c97fa2b0aeaa25bf2a8d5
ec221013 <ec221013@qmul.ac.uk>
2024-03-22 03:06:29 +0000

    wip

--- src/components/Weather-Daily.js
+++ src/components/Weather-Daily.js
@@ -7,23 +7,28 @@ import './Weather.css';
 
 const DailyWeather = () => {
 
-  const [tenDayWeather, setTenDayWeather] = useState([]);
+
+  const [tenDayWeather, setTenDayWeather] = useState([]); // Declare array variable with useState hook
   const city = 'London';
 
+  // Executes everything within when component is rendered
   useEffect(() => {
+    // Define function to fetch weather data
     const fetchData = async () => {
       try {
-        const data = await get10DayData(city);
+        const data = await get10DayData(city); // Function to use API call using city
         setTenDayWeather(data);
       } catch (error) {
         console.error('Error fetching weather data:', error.message);
       }
     };
 
-    fetchData();
+    
+    fetchData(); // Call the function to get weather data
   }, []);
 
-  const dayNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+  const dayNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']; // Array to convert date to corresponding weekday
 
   return (
     <div className="Wcontainer">
@@ -31,18 +36,21 @@ const DailyWeather = () => {
       <div className="Wlarge-container">
         <div className='Wbox-title'>10-Day Forecast</div>
 
-        
+        {/* Iterates over tenDayWeather array and creates following JSX for each item */}
         {tenDayWeather.map((day, index) => (
           <div className="W-entry" key={index}>
+            {/* Checks if item is the first item (today) or not and sets corresponding weekday*/}
             <div className='entry-day'>{index === 0 ? 'TODAY' : dayNames[new Date(day.date * 1000).getDay()]}</div>
             <div className='entry-icon'>
               <img src= {`https://openweathermap.org/img/wn/${day.icon}@2x.png`} alt='icon'/>
-              {/* Consult https://openweathermap.org/weather-conditions */}
+              {/* API Documentation https://openweathermap.org/weather-conditions */}
             </div>
             <div className='entry-temp'>
+              {/* Displays min and max temperatures of each day */}
               <div>{day.temp_min}°C / {day.temp_max}°C</div>
             </div>
             <div className='entry-desc'>
+              {/* Displays description of weather */}
               {day.desc}
             </div>
           </div>
